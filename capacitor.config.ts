f59eaae3de7367c9d3256e17b5925565500c36ce
MJMMiller <mateomiller1605@gmail.com>
2024-01-12 07:33:39 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'A3_S3_Miller_3E1',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
